b57ed88df2a5b73482245d0038c9ff0c611e1ea7
adityasahu6419-star <adityasahu6419@gmail.com>
2026-09-18 12:04:37 +0530

    Added the binary.cpp file to the repository
    This is the second change.

---
+++ BinarySystem.cpp
@@ -0,0 +1,39 @@
+// Decimal to binary converter 
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int decNum=122, rem, ans=0, pow=1;
+//     int x = decNum;
+//     while(decNum>0){
+//         rem = decNum%2;
+//         decNum=decNum/2;
+//         ans+=(rem*pow);
+//         pow*= 10;
+//     }
+
+//     cout << "The Binary of " << x << " is : " << ans << endl;
+
+//     return 0;
+// }
+
+// Binary to decimal converter
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int binNum=1101, rem, ans=0, pow=1;
+    int x = binNum;
+    while(binNum>0){
+        rem=binNum%10;
+        binNum/=10;
+        ans+=(rem*pow);
+        pow*=2;
+    }
+
+    cout << "The Decimal of " << x << " is : " << ans << endl;
+
+    return 0;
+}
